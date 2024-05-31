2d708a484d5d04dfb52c0ef1a5b13b3b3ec01a5b
ernesto <eszini@gmail.com>
2024-05-31 09:15:41 +0100

    tfor - agregue que cargue todos los files a cadenas de structs

---
+++ wrk7_use_examples/correr.sh
@@ -0,0 +1,8 @@
+
+
+# ./tfor -v -opciones=d5  -prue=2 -inp=l1 -out=l_names -aux=run.log -dato=repo2 > log
+
+
+./tfor -v -opciones=d5  -exec=2 -inp=l1 -out=l_names -aux=run.log -dato=repo2 > log
+
+
