c24f4796fcf7f349033ffb7e6708ecc7aea125f0
ernesto <eszini@gmail.com>
2024-06-06 23:03:22 +0100

    Mejoras a tfor.c

---
+++ A_src/del_files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Definir el directorio raíz desde el cual empezar
+root_dir="$HOME/wrk/tnfor"
+
+# Recorrer el árbol de directorios y eliminar los archivos .exe
+find "$root_dir" -type f -name "*.exe" -exec echo "Eliminando: {}" \; -exec rm -f {} \;
+find "$root_dir" -type f -name "*.log" -exec echo "Eliminando: {}" \; -exec rm -f {} \;
+find "$root_dir" -type f -name "log*" -exec echo "Eliminando: {}" \; -exec rm -f {} \;
+
+echo "Todos los archivos .exe han sido eliminados desde $root_dir."
+
+
+
