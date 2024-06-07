33447a050f706b15f702d511599bf26a6f6211bf
ernesto <eszini@gmail.com>
2024-06-07 21:25:26 +0100

    2024_06_07 ordenando la casa

--- A_src/tfor.c
+++ A_src/tfor.c
@@ -9591,6 +9591,48 @@ char	*s;
 
 
 
+char	*fname(bname) 
+char	*bname;
+{
+    static char 	nombre_completo[MAXP];
+    char 		fecha_hora[MAXF];
+
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    /* Formatear la fecha y hora */
+    snprintf(fecha_hora, sizeof(fecha_hora), "_%02d%02d%02d_%02d%02d",
+		tm.tm_year % 100, 
+		tm.tm_mon + 1, 
+		tm.tm_mday,
+		tm.tm_hour,
+		tm.tm_min);
+
+    /* Crear el nombre completo */
+    snprintf(nombre_completo, sizeof(nombre_completo), "%s%s", bname, fecha_hora);
+
+    return nombre_completo;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
  * -----------------------------------------------------------------------------------
@@ -10878,6 +10920,50 @@ int main() {
 #endif
 
 
+
+#if 0
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+
+char* archivo_log(char *nombre_log) 
+{
+    static char nombre_completo[256];
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+
+    // Formatear la fecha y hora
+    char fecha_hora[20];
+    snprintf(fecha_hora, sizeof(fecha_hora), "_%02d%02d%02d_%02d%02d",
+             tm.tm_year % 100, tm.tm_mon + 1, tm.tm_mday,
+             tm.tm_hour, tm.tm_min);
+
+    // Crear el nombre completo
+    snprintf(nombre_completo, sizeof(nombre_completo), "%s%s", nombre_log, fecha_hora);
+
+    return nombre_completo;
+}
+
+int main()
+{
+    char nombre_log[256];
+    strcpy(nombre_log, "prueba");
+
+    strcpy(nombre_log, archivo_log(nombre_log));
+    printf("Nombre del archivo: %s\n", nombre_log);
+
+    return 0;
+}
+
+
+
+
+#endif
+
+
 /*
  * -----------------------------------------------------------------------------------
  *	end of source  ( I hope)
@@ -10885,5 +10971,8 @@ int main() {
  *	end of source
  * -----------------------------------------------------------------------------------
  */
+
+
+
 
 
---
+++ wrk1_load_all_files/p1_todos.sh
@@ -0,0 +1,6 @@
+
+
+
+
+
+ ./tfor -v -opciones=d5 -prue=2 -inp=lr1 -out=l_names -aux=run.log -dato=new_repo  > log
