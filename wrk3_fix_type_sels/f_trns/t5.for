5284c9d9d617e38bbf9b979bf3bfffdb88341b3e
ernesto <eszini@gmail.com>
2024-06-11 04:11:07 +0100

    arreglos a tfor.c

---
+++ wrk3_fix_type_sels/f_trns/t5.for
@@ -0,0 +1,32 @@
+!     INCLUDE 'LAMCOM.MON'
+!     CHARACTER*20 MONTH_NAME
+!    +(12)/'January','February','March','April',
+!    +         'May','June','July','August','September','October',
+!    +         'November','December'/
+!     CHARACTER*9 DAY_NAME(3)/' weekdays',' weekends',' peakdays'/
+!     CHARACTER*11 CLASS_NAME(7)/'Commerical','Residential',
+!    +          'Industrial','Other 1','Other 2','Other 3','System'/
+!     INTEGER*2 SEASON,DAY,HOUR,LRECL,
+!    +          YEAR,CLASS,END_POINT,THREE_DAYS,TOTAL_DAYS
+
+
+!        1         2         3         4         5         6         7         8
+!2345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+     integer*2 pepe
+
+
+     integer*2 :: pepe
+
+
+     integer (kind=2) pepe
+
+
+     integer (kind=2)pepe
+
+
+     integer (kind=2) :: pepe
+
+
+
+     character (len=10)pepe
