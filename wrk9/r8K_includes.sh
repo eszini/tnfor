f6b88bfeb51c79ba50cd318ac85186fcc3651ee3
ernesto <eszini@gmail.com>
2024-07-13 17:19:12 +0100

    tfor - nuevas funciones para reporting de mons

---
+++ wrk9/r8K_includes.sh
@@ -0,0 +1,10 @@
+#
+#
+#
+# saca el listado de files - includes en p.log
+# utitliza MIDAS k
+#
+
+
+ ./tfor -v -opciones=d5 -exec=3 -inp=lr2 -out=kp.out -dato=repo4 -aux=kp.aux -log=kp.log  > log8k
+
