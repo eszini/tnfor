cd7b920ec87009ea691d8057caae87db807ec322
ernesto <eszini@gmail.com>
2024-07-27 16:22:42 +0100

    limpieza

--- wrk9_entrys/analis/t1_k.sh
+++ wrk9_entrys/analis/t1_k.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Leer el archivo jp.out y extraer la primera columna, eliminar duplicados y ordenar
-awk '{print $1}' kp.out | sort -u > z1_inc_k_temp
+awk '{print $1}' r_kres.txt | sort -u > z1_inc_k_temp
 
 # Formatear el archivo z1_inc_k_temp para tener "   " (tres espacios) antes de cada archivo
 awk '{print "   " $0}' z1_inc_k_temp > z1_inc_k
--- wrk9_entrys/p5_lco.sh
+++ wrk9_entrys/p5_lco.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clear_file="clear.txt"
+clear_file="clear.cfg"
 parser_log="parser.err"
 statis_log="tool.sta"
 checks_log="check.log"
@@ -8,7 +8,7 @@ checks_log="check.log"
 arc1="t5.for"
 arc2="t6.for"
 
-# Borrar pantalla segun clear.txt .... 
+# Borrar pantalla segun clear.cfg .... 
 if [ -f "$clear_file" ]; then
     # Obtener la línea que contiene "clear" y extraer el valor después del "="
     clear_option=$(grep -i '^\s*clear\s*=' "$clear_file" | sed 's/^\s*clear\s*=\s*//i')
