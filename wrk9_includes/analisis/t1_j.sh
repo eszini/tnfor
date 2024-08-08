b330338d91b6461a35076ec698ec6089a8223df9
ernesto <eszini@gmail.com>
2024-08-08 02:53:55 +0100

    tfor - agregados

--- wrk9_includes/analisis/t1_j.sh
+++ wrk9_includes/analisis/t1_j.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Leer el archivo jp.out y extraer la primera columna, eliminar duplicados y ordenar
-awk '{print $1}' jp.out | sort -u > z1_inc_j_temp
+# Leer el archivo r_jres.txt y extraer la primera columna, eliminar duplicados y ordenar
+awk '{print $1}' r_jres.txt | sort -u > z1_inc_j_temp
 
 # Formatear el archivo z1_inc_j_temp para tener "   " (tres espacios) antes de cada archivo
 awk '{print "   " $0}' z1_inc_j_temp > z1_inc_j
--- wrk9_includes/old/r8_includes.sh
+++
@@ -1,9 +0,0 @@
-#
-#
-#
-# saca el listado de files - includes en p.log
-#
-
-
- ./tfor -v -opciones=d5 -exec=3 -inp=lr1 -out=p.out -dato=repo4 -aux=p.aux -log=p.log  > log8
-
