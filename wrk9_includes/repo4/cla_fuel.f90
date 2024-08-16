6bff2cfdb0b6591454ddb3a7c4850cc873adf5f2
ernesto <eszini@gmail.com>
2024-08-16 04:59:06 +0100

    tfor - agregamos funcionalidad para vcb

--- wrk9_includes/repo4/cla_fuel.f90
+++
@@ -1,5 +0,0 @@
-module cla_fuel
-      real (kind=4), allocatable :: P_FUEL_DELIVERY_2(:) 
-      real (kind=4), allocatable :: P_FUEL_DELIVERY_1(:)
-      real (kind=4), allocatable :: P_FUEL_DELIVERY_3(:)
-end module cla_fuel
