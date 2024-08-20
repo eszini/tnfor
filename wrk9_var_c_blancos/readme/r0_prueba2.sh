8502fbc9d5c019697fcbbae4bebcb9f4aa6f3c50
ernesto <eszini@gmail.com>
2024-08-20 23:24:55 +0100

    tfor - actualizado para trabajar con -opt=1 2 3 ...

--- wrk9_var_c_blancos/readme/r0_prueba2.sh
+++ wrk9_var_c_blancos/readme/r0_prueba2.sh
@@ -14,5 +14,5 @@
 # lr1_todos_sin_mth  todos los files, menos los que tienen include
 
 
-./tfor -v -opciones=d5 -exec=6 -opt=1 -inp=lr1_solo_mth  -in2=vcb.txt -in3=vsb.txt -dato=repo4 -out=m1 -aux=m2 -log=m3 
+./tfor -v -opciones=d5 -exec=6 -opt=1 -inp=lr1_todos  -in2=vcb.txt -in3=vsb.txt -dato=repo4 -out=m1 -aux=m2 -log=m3 
 
